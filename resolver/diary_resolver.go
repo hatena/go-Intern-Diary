aa0da8c1a1c79533540dc5bdfb9e83b87892882d
加藤郁之 <fumilemon79@gmail.com>
2019-05-08 23:52:24 +0900

    loaderを使用しない愚直な実装

--- resolver/diary_resolver.go
+++ resolver/diary_resolver.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/graph-gophers/graphql-go"
 	"github.com/hatena/go-Intern-Diary/model"
+	"github.com/hatena/go-Intern-Diary/service"
 )
 
 type diaryResolver struct {
 	diary *model.Diary
+	app   service.DiaryApp
 }
 
 func (d *diaryResolver) ID(ctx context.Context) graphql.ID {
@@ -20,6 +22,16 @@ func (d *diaryResolver) Name(ctx context.Context) string {
 	return d.diary.Name
 }
 
-// func (d *diaryResolver) Articles(ctx context.Context) []*articleResolver {
-
-// }
+func (d *diaryResolver) Articles(ctx context.Context) ([]*articleResolver, error) {
+	page := uint64(1)
+	limit := uint64(100)
+	articles, err := d.app.ListArticlesByDiaryID(d.diary.ID, page, limit)
+	if err != nil {
+		return nil, err
+	}
+	ars := make([]*articleResolver, len(articles))
+	for i, article := range articles {
+		ars[i] = &articleResolver{article}
+	}
+	return ars, nil
+}
--- resolver/user_resolver.go
+++ resolver/user_resolver.go
@@ -23,5 +23,15 @@ func (u *userResolver) Name(ctx context.Context) string {
 }
 
 func (u *userResolver) Diaries(ctx context.Context) ([]*diaryResolver, error) {
-
+	page := uint64(1)
+	limit := uint64(100) // todo
+	diaries, err := u.app.ListDiariesByUserID(u.user.ID, page, limit)
+	if err != nil {
+		return nil, err
+	}
+	diaryResolvers := make([]*diaryResolver, len(diaries))
+	for i, diary := range diaries {
+		diaryResolvers[i] = &diaryResolver{diary, u.app}
+	}
+	return diaryResolvers, nil
 }
