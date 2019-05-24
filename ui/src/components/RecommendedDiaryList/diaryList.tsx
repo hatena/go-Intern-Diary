cc99b209bff553088818722e656a558370efe455
FumiyukiKato <fumilemon79@gmail.com>
2019-05-24 19:16:31 +0900

    コミットし忘れ

--- ui/src/components/RecommendedDiaryList/diaryList.tsx
+++ ui/src/components/RecommendedDiaryList/diaryList.tsx
@@ -1,22 +1,41 @@
 import React from "react"
 import { Query } from "react-apollo";
 
-import {query} from "./container"
+import {query, RecommendedDiary} from "./container"
+import {DiaryListItem} from "./diaryListItem"
 
 interface RecommendedDiaryListProps {
     diaryId: string;
 }
 
-
 export const RecommendedDiaryList: React.StatelessComponent<RecommendedDiaryListProps> = ({diaryId}) => {
     return (
         <div className="RecommendedDiaryList">
-            <Query<>
+            <Query<ListRecommendDiary, ListRecommendDiaryVariables>
                 query={query}
                 variables={{
                     diaryId: diaryId
                 }}
             >
+                {result =>{
+                    if (result.error) {
+                        if (result.error.message == "GraphQL error: please login") {
+                            return <a href="/">ログインして下さい</a>
+                        }
+                        return <p className="error">Error: {result.error.message}</p>                    
+                    }
+                    if (result.loading) {
+                        return <p className="loading">Loading</p>
+                    }
+                    const { data } = result;
+                    const recommendedDiaries = data!.
+                    {recommendedDiaries.map(diary => (
+                        <div>
+                            <DiaryListItem diary={diary} />
+                        </div>
+                    ))}
+                }
+                }}
             </Query>
         </div>
     )
