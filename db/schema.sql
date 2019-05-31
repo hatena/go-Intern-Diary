7f76714d8b35f1281b9fa143a2da93404e6c495f
FumiyukiKato <fumilemon79@gmail.com>
2019-05-31 19:01:46 +0900

    DBからカテゴリを消した

--- db/schema.sql
+++ db/schema.sql
@@ -62,20 +62,21 @@ CREATE TABLE tag (
     `created_at` DATETIME(6) NOT NULL,
     `updated_at` DATETIME(6) NOT NULL,
 
-    PRIMARY KEY (id),
-    UNIQUE KEY `tag_category_unique_index` (`tag_name`,`category_id`),
-    KEY(tag_name)
+    PRIMARY KEY (`id`),
+    -- UNIQUE KEY `tag_category_unique_index` (`tag_name`,`category_id`),
+    KEY(`tag_name`),
+    KEY(`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE category (
-    `id` BIGINT UNSIGNED NOT NULL,
+-- CREATE TABLE category (
+--     `id` BIGINT UNSIGNED NOT NULL,
 
-    `category_name` VARCHAR(255) NOT NULL,
+--     `category_name` VARCHAR(255) NOT NULL,
 
-    `created_at` DATETIME(6) NOT NULL,
-    `updated_at` DATETIME(6) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--     `created_at` DATETIME(6) NOT NULL,
+--     `updated_at` DATETIME(6) NOT NULL,
+--     PRIMARY KEY (id)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE diary_tag (
     `id` BIGINT UNSIGNED NOT NULL,
--- repository/diary.go
+++ repository/diary.go
@@ -267,42 +267,6 @@ func (r *repository) ListDiariesByTagIDs(tagIDs []uint64) (map[uint64][]*model.D
 	return diaries, nil
 }
 
-func (r *repository) ListRecommendedDiaries(diaryID uint64) ([]*model.Diary, error) {
-	tagsOfDiary, err := r.getTags(diaryID)
-	if err != nil {
-		return nil, err
-	}
-	diaries, err := r.sameTagDiaries(tagsOfDiary, diaryID)
-	if err != nil {
-		return nil, err
-	}
-	return diaries, nil
-}
-
-func (r *repository) sameTagDiaries(tags []*model.Tag, diaryID uint64) ([]*model.Diary, error) {
-	if len(tags) == 0 {
-		return nil, nil
-	}
-	tagIDs := make([]uint64, len(tags))
-	for i, tag := range tags {
-		tagIDs[i] = tag.ID
-	}
-	query, args, err := sqlx.In(
-		`SELECT DISTINCT diary.id, diary.name, diary.user_id, diary.updated_at FROM diary
-			JOIN diary_tag ON diary.id = diary_tag.diary_id
-			JOIN user ON diary.user_id = user.id
-			WHERE tag_id IN (?) AND diary.id != ?
-			ORDER BY diary.updated_at DESC LIMIT 3
-		`, tagIDs, diaryID,
-	)
-	if err != nil {
-		return nil, err
-	}
-	var diaries []*model.Diary
-	err = r.db.Select(&diaries, query, args...)
-	return diaries, err
-}
-
 func (r *repository) getTags(diaryID uint64) ([]*model.Tag, error) {
 	var tagsOfDiary []*model.Tag
 	err := r.db.Select(&tagsOfDiary, `
